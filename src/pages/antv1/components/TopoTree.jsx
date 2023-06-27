dffed1d3a8c301773a8a1bc0a4754db04a3e739b
dongmin <dongmin@honoe-win.cn>
2023-06-27 11:03:46 +0800

    提交

--- src/pages/antv1/components/TopoTree.jsx
+++ src/pages/antv1/components/TopoTree.jsx
@@ -5,34 +5,16 @@ import NodeToolTips from './NodeTooltips';
 import './TopoTree.less';
 import G6 from '@antv/g6-3.5';
 import 'src/assets/topo/iconfont/iconfont.css';
+import { stringify } from 'qs';
 
 const TopoTree = ({
   tachInfo,
+  topoType,
   topologyData,
+  topoTreeLoading,
   setTopoTreeLoading,
+  tacheParam,
 }) => {
-
-  const collapseIcon = (x, y, r) => {
-    return [
-      ['M', x - r, y],
-      ['a', r, r, 0, 1, 0, r * 2, 0],
-      ['a', r, r, 0, 1, 0, -r * 2, 0],
-      ['M', x - r + 4, y],
-      ['L', x - r + 2 * r - 4, y],
-    ];
-  };
-  const expandIcon = (x, y, r) => {
-    return [
-      ['M', x - r, y],
-      ['a', r, r, 0, 1, 0, r * 2, 0],
-      ['a', r, r, 0, 1, 0, -r * 2, 0],
-      ['M', x - r + 4, y],
-      ['L', x - r + 2 * r - 4, y],
-      ['M', x - r + r, y - r + 4],
-      ['L', x, y + r - 4],
-    ];
-  };
-
   let graph = useRef();
 
   // 节点tooltip坐标
@@ -54,34 +36,6 @@ const TopoTree = ({
       topologyData.edges.length > 0 &&
       topologyData.nodes.length > 0
     ) {
-      console.log('topologyData', topologyData)
-      let data = {  //定义一个数组就是这组图中需要传入的数据，包含节点、分组及边的信息
-        nodes: [
-          { id: 'node2', x: 300, y: 200, label: 'i系统', comboId: 'combo2' },
-          { id: 'node4', x: 300, y: 300, label: 'i系统', comboId: 'combo4' },
-          { id: 'node5', x: 300, y: 400, label: 'i系统', comboId: 'combo4' },
-  
-          { id: 'node3', x: 100, y: 200, label: '数据工厂系统', comboId: 'combo3' },
-          { id: 'node1', x: 500, y: 200, label: '数据工厂系统' },
-  
-        ],
-        combos: [
-          { id: 'combo2', label: 'Combo 2' },
-          { id: 'combo3', label: 'Combo 3' },
-          // { id: 'combo1', label: 'Combo 1' },
-          { id: 'combo4', label: 'Combo 4', parentId: 'combo2' },
-        ],
-        edges: [
-          {
-            source: 'node3',
-            target: 'node5',
-          },
-          {
-            source: 'node1',
-            target: 'node2',
-          },
-        ],
-      }
       initTopo(topologyData);
     }
   }, [topologyData]);
@@ -113,7 +67,7 @@ const TopoTree = ({
         ranksep: 10,
         nodesep: 5,
         controlPoints: true,
-        nodesepFunc: d => 50, //元素之间的间距
+        nodesepFunc: d => 50,
         ranksepFunc: d => 160,
       },
       defaultNode: {
@@ -156,7 +110,6 @@ const TopoTree = ({
       },
       modes: {
         default: [
-          // 'drag-combo',
           'drag-canvas',
           {
             type: 'drag-node',
@@ -167,7 +120,9 @@ const TopoTree = ({
             // sensitivity: 2,
             minZoom: 0,
           },
+          { type: "drag-combo", enableOptimize: true, onlyChangeComboSize: true },
         ],
+        // default: ['drag-node', 'drag-canvas', 'zoom-canvas'],
       },
       groupByTypes: false,
       defaultCombo: {
@@ -180,13 +135,54 @@ const TopoTree = ({
         }
       },
     });
-
     graph.current.data(nodeData);
     graph.current.render();
     graph.current.fitView();
     graph.current.get('canvas').set('supportCSSTransform', true);
     // 图谱节点双击事件
     graph.current.on('node:dblclick', ev => {
+      // 跳转至根因分析
+      const { name, systemCode } = ev.item._cfg.model;
+      const commonParams = {
+        systemCode,
+        serviceName: name,
+        startTime: tacheParam.startTime,
+        endTime: tacheParam.endTime,
+        dateSwitch: '1',
+      };
+
+      let ip = document.referrer;
+      if (topoType === 'c') {
+        // if (process.env.UMI_ENV === 'core' || !process.env.UMI_ENV) {
+        ip = window.location.origin;
+        window.open(`${ip}/callchain/#/callchain/callChain/LogSearch?${stringify(commonParams)}`);
+        // } else {
+        //   window.open(
+        //     `${ip}#/thirdPartyPlatform/fullLink?menuModule=${encodeURIComponent(
+        //       '全链路日志平台'
+        //     )}&menuTitle=${encodeURIComponent('C系统日志搜索')}&thirdLink=${encodeURIComponent(
+        //       `${jumpWindowIp}/callchain/#/callchain/callchain/LogSearch`
+        //     )}?${qs.stringify(commonParams)}`
+        //   );
+        // }
+      } else {
+        // if (process.env.UMI_ENV === 'core' || !process.env.UMI_ENV) {
+        ip = window.location.origin;
+        window.open(
+          `${ip}/callchain/#/callchain/callChain/serviceRootCauseCrossSystem?${stringify(
+            commonParams
+          )}`
+        );
+        // } else {
+        //   window.open(
+        //     `${ip}#/thirdPartyPlatform/fullLink?menuModule=${encodeURIComponent(
+        //       '全链路日志平台'
+        //     )}&menuTitle=${encodeURIComponent('根因分析')}&thirdLink=${encodeURIComponent(
+        //       `${jumpWindowIp}/callchain/#/callchain/callchain/serviceRootCauseCrossSystem`
+        //     )}?${qs.stringify(commonParams)}`
+        //   );
+        // }
+      }
     });
 
     graph.current.on('node:mouseenter', ev => {
@@ -201,7 +197,6 @@ const TopoTree = ({
       // 节点tooltip效果
       const { item } = ev;
       const model = item.getModel();
-      console.log(model)
       const { x, y } = model;
       const point = graph.current.getCanvasByPoint(x, y);
       setNodeInfo(ev.item._cfg.model);
@@ -221,106 +216,17 @@ const TopoTree = ({
       setShowNodeTooltip(false);
     });
 
-    // 点击combo的收缩icon
-    graph.current.on('combo:click', e => {
-      if (e.target.get('name') === 'combo-marker-shape') {
-        // graph.current.collapseExpandCombo(e.item.getModel().id);
-        graph.current.collapseExpandCombo(e.item);
-        // if (graph.current.get('layout')) graph.current.layout();
-        // else graph.current.refreshPositions();
-      }
-    });
-  
-    graph.current.on('combo:dragend', e => {
-      graph.current.getCombos().forEach(combo => {
-        graph.current.setItemState(combo, 'dragenter', false);
-      })
-    });
-    graph.current.on('node:dragend', e => {
-      graph.current.getCombos().forEach(combo => {
-        graph.current.setItemState(combo, 'dragenter', false);
-      })
-    });
-  
-    graph.current.on('combo:dragenter', e => {
-      graph.current.setItemState(e.item, 'dragenter', true);
-    });
-    graph.current.on('combo:dragleave', e => {
-      graph.current.setItemState(e.item, 'dragenter', false);
-    });
-
+    // 双击combo
     graph.current.on("combo:dblclick", (e) => {
-      // e.item._cfg.model.type = e.item._cfg.model.type === "rect" ? "circle" : "rect";  // 分组形状，方圆切换
-      // e.item._cfg.model.labelCfg.refY = e.item._cfg.model.type === "rect" ? -20 : 5;   // 切换形状，改变label定位
-
       const comboId = e.item._cfg.model.id;
-      console.log(comboId)
+      // 展开或收缩指定的combo
       graph.current?.collapseExpandCombo(comboId || '');
-
-      // 分组收缩时,删除分组内的连线和节点
-      if (e.item._cfg.model.collapsed) {   // 收缩 
-        let newedges = e.item.getEdges();
-        let newNodes = e.item.getNodes();
-        for (let j = 0; j < newedges.length; j++) {
-          graph.current.removeItem(newedges[j]);
-        }
-        for (let i = 0; i < newNodes.length; i++) {
-          graph.current.removeItem(newNodes[i]);
-        }
-        topologyData.edges.forEach(edge => {
-          graph.current.addItem("edge", edge);
-        });
-      } else {   // 展开
-        // 分组展开时, 添加节点和连线，并给分组内的节点 添加位置信息
-        let origin = [e.item._cfg.model.x, e.item._cfg.model.y];  // 获取当前分组combs的坐标
-        let row = 110, clo = 150;
-        // 生成(10-20)随机数个 随机数 模拟展开分组内的节点
-        let randomCount = Math.floor(Math.random() * 10) + 10;
-        let row_clo = Math.floor(Math.sqrt(randomCount));
-        let nodes = []
-        for (let i = 0; i < randomCount; i++) {
-          let min = comboId.split('-')[0] - 0
-          let max = comboId.split('-')[1] - 0
-          let randomNum = Math.floor(Math.random() * (max - min)) + min;
-          if (nodes.indexOf(randomNum) > -1) {
-            i--
-            continue;
-          }
-          nodes.push(randomNum)
-          let rowindex = Math.floor(i / row_clo);
-          let cloindex = i % row_clo;
-          let y = origin[1] + row * rowindex
-          let node = {
-            label: randomNum,
-            id: randomNum.toString(),
-            comboId: comboId,
-            style: {
-              fillOpacity: 0.5,
-              cursor: "pointer",
-              fill: randomNum % 5 == 0 ? "#81C7D4" : "#986DB2"
-            },
-            x: origin[0] + clo * cloindex,
-            y: i % 2 == 0 ? y + 40 : y
-          }
-          graph.current.addItem("node", node); // 将节点添加至分组 
-        }
-        nodes.sort((a, b) => a - b)  // 将分组内的数字排序，从小到大依次连接，模拟真实数据
-        for (let i = 0; i < nodes.length - 1; i++) {
-          let edge = {
-            source: nodes[i].toString(),
-            target: nodes[i + 1].toString(),
-            lineWidth: 1,
-            style: {
-              lineDash: [2, 2],
-              lineWidth: 0.5,
-              stroke: "#00AA90"
-            }
-          }
-          graph.current.addItem("edge", edge);  // 添加连线   将分组内的数字排序，从小到大依次连接
-        }
-      }
     });
 
+    // 初始化时，combos为收缩状态
+    topologyData.combos.map(combos=>{
+      // graph.current?.collapseExpandCombo(combos.id || '');
+    })
   };
 
   const registerNode = () => {
@@ -464,18 +370,35 @@ const TopoTree = ({
           });
           return shape;
         },
+        // setState(name, value, item) {
+        //   alert('setState2');
+
+        //   const group = item.getContainer();
+        //   const shape = group.get('children')[0];
+        //   switch (name) {
+        //     case 'selected':
+        //     case 'inSelected':
+        //     case 'outSelected':
+        //       {
+        //         if (value) {
+        //           shape.attr('fill', '#6C7B7E');
+        //         } else {
+        //           shape.attr('fill', 'transparent');
+        //         }
+        //       }
+        //       break;
+        //   }
+        // },
       },
       'rect'
     );
 
     G6.registerCombo('cRect', {
       drawShape: function drawShape(cfg, group) {
+        console.log('cRect', group)
         const self = this;
-        // Get the padding from the configuration
         cfg.padding = cfg.padding || [50, 20, 20, 20];
-        // Get the shape's style, where the style.width and style.height correspond to the width and height in the figure of Illustration of Built-in Rect Combo
         const style = self.getShapeStyle(cfg);
-        // Add a rect shape as the keyShape which is the same as the extended rect Combo
         const rect = group.addShape('rect', {
           attrs: {
             ...style,
@@ -487,40 +410,12 @@ const TopoTree = ({
           draggable: true,
           name: 'combo-keyShape'
         });
-        // 展开收缩的icon
-        // group.addShape('marker', {
-        //   attrs: {
-        //     ...style,
-        //     fill: '#fff',
-        //     opacity: 1,
-        //     // cfg.style.width and cfg.style.heigth correspond to the innerWidth and innerHeight in the figure of Illustration of Built-in Rect Combo
-        //     x: cfg.style.width / 2 + cfg.padding[1],
-        //     y: (cfg.padding[2] - cfg.padding[0]) / 2,
-        //     r: 10,
-        //     symbol: collapseIcon
-        //   },
-        //   draggable: true,
-        //   name: 'combo-marker-shape'
-        // });
         return rect;
       },
-      // Define the updating logic of the right circle
       afterUpdate: function afterUpdate(cfg, combo) {
-        console.log(cfg.collapsed,combo)
-        const group = combo.get('group');
-        // Find the circle shape in the graphics group of the Combo by name
-        const marker = group.find(ele => ele.get('name') === 'combo-marker-shape');
-        // Update the position of the right circle
-        // marker.attr({
-        //   // cfg.style.width and cfg.style.heigth correspond to the innerWidth and innerHeight in the figure of Illustration of Built-in Rect Combo
-        //   x: cfg.style.width / 2 + cfg.padding[1],
-        //   y: (cfg.padding[2] - cfg.padding[0]) / 2,
-        //   // The property 'collapsed' in the combo data represents the collapsing state of the Combo
-        //   // Update the symbol according to 'collapsed'
-        //   symbol: cfg.collapsed ? expandIcon : collapseIcon
-        // });
       }
     }, 'rect');
+
   };
   const setNodeIcon = (type, isWarning, objType) => {
     let nodeIcon = [];
@@ -625,10 +520,10 @@ const TopoTree = ({
   };
 
   return (
-    <Spin size="large" wrapperClassName='containerSpin' spinning={setTopoTreeLoading}>
-      <Card className='card' title={<span>【{tachInfo.systemName}】层级拓扑</span>}>
-        <div className='topoTree'>
-          <div key="2" id="topology" className='topoTreeMain'>
+    <Spin size="large" wrapperClassName={'containerSpin'} spinning={setTopoTreeLoading}>
+      <Card className={'card'} title={<span>【{tachInfo.systemName}】层级拓扑</span>}>
+        <div className={'topoTree'}>
+          <div key="2" id="topology" className={'topoTreeMain'}>
             {showNodeTooltip && (
               <NodeToolTips nodeInfo={nodeInfo} x={nodeTooltipX} y={nodeTooltipY} />
             )}
